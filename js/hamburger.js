e800c91f09a96513d039648b8dd06b212af46a35
Иван <i.tarchevsky@gmail.com>
2021-11-15 12:16:55 +0300

    Фоны семинаров, js hamburger, css hamburger, кнопка Связаться в меню

--- js/hamburger.js
+++ js/hamburger.js
@@ -5,8 +5,10 @@
 let ham = document.querySelector('.hamburger');
 let nav = document.querySelector('.nav');
 let hamactive = document.querySelector('.hamburger__active');
+let body = document.querySelector('body');
 
 ham.onclick = function() {
 	ham.classList.toggle('hamburger__active');
 	nav.classList.toggle('nav__active');
+	body.classList.toggle('body__hidden')
 };
